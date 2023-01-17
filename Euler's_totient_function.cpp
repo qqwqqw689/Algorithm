0c3dd782810e69eedc692df844815f87ae5fd753
qqwqqw689 <114795525+qqwqqw689@users.noreply.github.com>
2023-01-17 21:10:41 +0800

    Create Euler's_totient_function.cpp

---
+++ Euler's_totient_function.cpp
@@ -0,0 +1,30 @@
+// https://cp-algorithms.com/algebra/phi-function.html
+// Euler's totient function
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int phi(int n)
+{
+    int result=n;
+    for(int i=2;i*i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            while(n%i==0)
+                n/=i;
+            result-=result/i;
+        }
+    }
+    if(n>1)
+        result-=result/n;
+
+    return result;
+}
+
+int main()
+{
+    cout<<phi(21);
+
+    return 0;
+}
