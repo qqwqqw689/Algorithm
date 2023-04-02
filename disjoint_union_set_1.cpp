0d5fd3ac160db55398a3b3c506145b65c7526039
qqwqqw689 <114795525+qqwqqw689@users.noreply.github.com>
2023-04-02 20:38:35 +0800

    Create disjoint_union_set_1.cpp

---
+++ disjoint_union_set_1.cpp
@@ -0,0 +1,48 @@
+// Disjoint set Union (support distances up to representative)
+// https://cp-algorithms.com/data_structures/disjoint_set_union.html#support-distances-up-to-representative
+// application: support dstances up to representative
+
+#include <bits/stdc++.h>
+#define N 10
+
+std::pair<int, int> parent[N];
+
+int rank[N];
+
+// creates a new set consisting of the new element v
+void make_set(int v) {
+    parent[v] = std::make_pair(v, 0);
+    rank[v] = 0;
+}
+
+// find_set(v) - returns the representative (also called leader) of the set that contains the element v
+// Path compression optimization
+
+std::pair<int, int> find_set(int v) {
+    /*
+    return <the representative of the set, the distance to it>
+    */
+    if(v != parent[v].first) {
+        int len = parent[v].second;
+        parent[v] = find_set(parent[v].first);
+        parent[v].second += len;
+    }
+    return parent[v];
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a).first;
+    b = find_set(b).first;
+    if (a != b) {
+        if (rank[a] < rank[b])
+            std::swap(a, b);
+        parent[b] = std::make_pair(a, 1);
+        if (rank[a] == rank[b])
+            rank[a]++;
+    }
+}
+
+int main()
+{
+    return 0;
+}
