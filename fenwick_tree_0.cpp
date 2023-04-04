2b7595ca7badb27e73dfab19f6376318cde2716e
qqwqqw689 <114795525+qqwqqw689@users.noreply.github.com>
2023-04-04 15:22:03 +0800

    Finding sum in one-dimensional array

--- fenwick_tree_0.cpp
+++ fenwick_tree_0.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 // https://cp-algorithms.com/data_structures/fenwick.html#finding-sum-in-one-dimensional-array
-// fenwick Tree Finding sum in one-dimensional array
+// Finding sum in one-dimensional array
 
 struct FenwickTree {
     std::vector<int> bit; // binary indexed tree bit[i] = sum(A[g(i),i])
@@ -12,13 +12,12 @@ struct FenwickTree {
     }
 
     FenwickTree(std::vector<int> const &a) : FenwickTree(a.size()) {
-        for (size_t i = 0; i < a.size(); i++)
-            add(i, a[i]);
-    }
-
-    void add(int idx, int delta) {
-        for(; idx < n; idx = idx | (idx + 1))
-            bit[idx] += delta;
+        for (size_t i = 0; i < n; i++) {
+            bit[i] += a[i]; // linear construction O(N)
+            int r = i | (i+1);
+            if (r < n) bit[r] += bit[i];
+        }
+            
     }
 
     int sum(int r) {
